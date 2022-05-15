3bcd2b5393b1e5108fb2b012ec989a958672d695
matt <matt@second-hands.com>
2022-05-15 21:37:40 +0900

    4장 privacy

--- money.py
+++ money.py
@@ -1,9 +1,9 @@
 class Dollor:
     def __init__(self, amount) -> None:
-        self.amount = amount
+        self.__amount = amount
 
     def times(self, multiplier):
-        return Dollor(self.amount * multiplier)
+        return Dollor(self.__amount * multiplier)
 
     def __eq__(self, __o: object) -> bool:
-        return self.amount == __o.amount
+        return self.__amount == __o.__amount
