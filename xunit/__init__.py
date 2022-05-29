f7496fb0f45fcf5a7335ab711eb617233f33fa9c
matt <matt@second-hands.com>
2022-05-30 00:02:18 +0900

    Chap.19 테스트를 하다보면 발생하는 3가지 패턴 (Bill Wake의 3A)
    Arrange - 객체를 생성
    Act - 어떤 자극
    Assert - 결과를 검사
    2,3 단계는 다르지만 1단계는 공통으로 가져가는 경우가 있다.
    
    ex) 객체 7,3을 통한 덧셈과 뺄셈
    
    객체를 생성할 때 두 가지를 고려한다.
    
    성능:
    여러 테스트가 같은 객체를 사용하면, 효율적이다.
    격리:
    한 테스트가 다른 테스트의 결과에 영향을 미치지 않아야한다.

--- xunit/__init__.py
+++ xunit/__init__.py
@@ -1,27 +1,57 @@
+import sys
+
+
+def callersname():
+    return sys._getframe(2).f_code.co_name
+
+
 class TestCase:
     def __init__(self, name):
+        print("TestCase __init__ name= ", name)
         self.name = name
 
+    def setUp(self):
+        pass
+
     def run(self):
+        print("TestCase run()")
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
 
-class wasRun(TestCase):
+class WasRun(TestCase):
     def __init__(self, name):
+        print(f"{self.__class__.__name__}::__init__()")
         self.wasRun = None
         TestCase.__init__(self, name)
 
     def testMethod(self):
+        print(f"{self.__class__.__name__}::testMethod()")
         self.wasRun = 1
+
+    def setUp(self):
+        print(f"{self.__class__.__name__}::setUp()")
+        self.wasRun = None
+        self.wasSetUp = 1
 
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+
+        print(f"{self.__class__.__name__}::setUp()")
+        self.test = WasRun("testMethod")
+
     def testRunning(self):
-        test = wasRun("testMethod")
-        assert not test.wasRun
-        test.run()
-        assert test.wasRun
+        print(f"{self.__class__.__name__}::testRunning()")
+        self.test.run()
+        assert self.test.wasRun
+
+    def testSetUp(self):
+        print(f"{self.__class__.__name__}::testSetUp()")
+        self.test.run()
+        assert self.test.wasSetUp
 
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
